906d103b91dc437b54a454cf25aaad605507f0fe
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-08-26 15:11:49 +1000

    fix external tables

--- models/external/mainnet/canonical_beacon_block.sql
+++ models/external/mainnet/canonical_beacon_block.sql
@@ -6,8 +6,8 @@ cache:
   full_scan_interval: 24h
 ---
 SELECT 
-    min(block_number) as min,
-    max(block_number) as max
+    min(slot_start_date_time) as min,
+    max(slot_start_date_time) as max
 -- Use the default database as predicate pushdown does not work with views.
 -- This gives 2-3x the performance.
 -- Once we move the data into the mainnet database, we no longer need this.
@@ -16,7 +16,7 @@ WHERE
     meta_network_name = 'mainnet'
 {{ if .cache.is_incremental_scan }}
     AND (
-      block_number <= {{ .cache.previous_min }}
-      OR block_number >= {{ .cache.previous_max }}
+      slot_start_date_time <= fromUnixTimestamp({{ .cache.previous_min }})
+      OR slot_start_date_time >= fromUnixTimestamp({{ .cache.previous_max }})
     )
 {{ end }}
