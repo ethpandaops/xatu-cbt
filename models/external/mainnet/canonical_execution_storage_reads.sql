bc6a233c3178daf878819d6e94e53179f82f546d
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-08-26 10:06:40 +1000

    feat: optimize external models with default database and adjusted scan intervals
    - Change incremental scan interval from 30s to 1m for all canonical execution models
    - Use default database instead of mainnet for 2-3x performance improvement
    - Fixes predicate pushdown limitations with views
    - Update intermediate address access models accordingly

--- models/external/mainnet/canonical_execution_storage_reads.sql
+++ models/external/mainnet/canonical_execution_storage_reads.sql
@@ -2,14 +2,21 @@
 database: mainnet
 table: canonical_execution_storage_reads
 cache:
-  incremental_scan_interval: 30s
+  incremental_scan_interval: 1m
   full_scan_interval: 24h
 ---
 SELECT 
     min(block_number) as min,
     max(block_number) as max
-FROM `{{ .self.database }}`.`{{ .self.table }}`
+-- Use the default database as predicate pushdown does not work with views.
+-- This gives 2-3x the performance.
+-- Once we move the data into the mainnet database, we no longer need this.
+FROM `default`.`{{ .self.table }}`
+WHERE 
+    meta_network_name = 'mainnet'
 {{ if .cache.is_incremental_scan }}
-WHERE block_number <= {{ .cache.previous_min }}
-   OR block_number >= {{ .cache.previous_max }}
+    (
+      block_number <= {{ .cache.previous_min }}
+      OR block_number >= {{ .cache.previous_max }}
+    )
 {{ end }}
