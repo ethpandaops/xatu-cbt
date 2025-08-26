f3ff578b202168f21d41a833d4f5963d98c24749
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-08-26 15:45:16 +1000

    convert external slot time to timestamp

--- models/external/mainnet/canonical_beacon_proposer_duty.sql
+++ models/external/mainnet/canonical_beacon_proposer_duty.sql
@@ -6,8 +6,8 @@ cache:
   full_scan_interval: 24h
 ---
 SELECT 
-    min(slot_start_date_time) as min,
-    max(slot_start_date_time) as max
+    toUnixTimestamp(min(slot_start_date_time)) as min,
+    toUnixTimestamp(max(slot_start_date_time)) as max
 -- Use the default database as predicate pushdown does not work with views.
 -- This gives 2-3x the performance.
 -- Once we move the data into the mainnet database, we no longer need this.
