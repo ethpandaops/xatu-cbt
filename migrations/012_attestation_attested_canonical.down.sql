592c1c0fec7bec214b1f8fe5535efaa9ea2f6ad5
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-09-15 15:17:05 +1000

    down migration

--- migrations/012_attestation_attested_canonical.down.sql
+++ migrations/012_attestation_attested_canonical.down.sql
@@ -1,2 +1,8 @@
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_attestation_attested_canonical ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_attestation_attested_canonical_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_correctness_canonical ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_correctness_canonical_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_correctness_by_validator_head ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_correctness_by_validator_head_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_correctness_by_validator_canonical ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_correctness_by_validator_canonical_local ON CLUSTER '{cluster}';
