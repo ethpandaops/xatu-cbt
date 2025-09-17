a59675dca1320ce601b4d17a0798f6e76514967e
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-09-17 12:24:27 +1000

    convert to dim_node

--- migrations/013_entity.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_entity ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_entity_local ON CLUSTER '{cluster}';
---
+++ migrations/013_node.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.dim_node ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.dim_node_local ON CLUSTER '{cluster}';
