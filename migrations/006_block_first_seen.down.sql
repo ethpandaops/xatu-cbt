06b5981cdc3070f27d75571ad175108d6ed0b925
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-09-18 11:36:49 +1000

    refactor: rename int_block_first_seen_by_node to fct_block_first_seen_by_node (#48)
    - Update table names to follow fact table naming convention
    - Add deduplicate_merge_projection_mode setting
    - Add p_by_slot projection for query optimization
    - Update all related protobuf definitions and tests

--- migrations/006_block_first_seen.down.sql
+++ migrations/006_block_first_seen.down.sql
@@ -1,2 +1,2 @@
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_block_first_seen_by_node ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_block_first_seen_by_node_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_block_first_seen_by_node ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_block_first_seen_by_node_local ON CLUSTER '{cluster}';
