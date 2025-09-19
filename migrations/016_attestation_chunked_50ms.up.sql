30d0af79913c2ca29e96a0bdfbfc64d03a84a895
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-09-19 11:00:06 +1000

    feat: add attestation first seen chunked 50ms tracking model (#53)
    * feat: add attestation first seen chunked 50ms tracking model
    
    * fix overrides
    
    * median ci
    
    * use gh hosted runners

---
+++ migrations/016_attestation_chunked_50ms.up.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `${NETWORK_NAME}`.fct_attestation_first_seen_chunked_50ms_local on cluster '{cluster}' (
+    `updated_date_time` DateTime COMMENT 'Timestamp when the record was last updated' CODEC(DoubleDelta, ZSTD(1)),
+    `slot` UInt32 COMMENT 'The slot number' CODEC(DoubleDelta, ZSTD(1)),
+    `slot_start_date_time` DateTime COMMENT 'The wall clock time when the slot started' CODEC(DoubleDelta, ZSTD(1)),
+    `epoch` UInt32 COMMENT 'The epoch number containing the slot' CODEC(DoubleDelta, ZSTD(1)),
+    `epoch_start_date_time` DateTime COMMENT 'The wall clock time when the epoch started' CODEC(DoubleDelta, ZSTD(1)),
+    `block_root` String COMMENT 'The beacon block root hash that was attested, null means the attestation was missed' CODEC(ZSTD(1)),
+    `chunk_slot_start_diff` UInt32 COMMENT 'The different between the chunk start time and slot_start_date_time. "1500" would mean this chunk contains attestations first seen between 1500ms 1550ms into the slot' CODEC(DoubleDelta, ZSTD(1)),
+    `attestation_count` UInt32 COMMENT 'The number of attestations in this chunk' CODEC(ZSTD(1))
+) ENGINE = ReplicatedReplacingMergeTree(
+    '/clickhouse/{installation}/{cluster}/tables/{shard}/{database}/{table}',
+    '{replica}',
+    `updated_date_time`
+) PARTITION BY toStartOfMonth(slot_start_date_time)
+ORDER BY
+    (`slot_start_date_time`, `block_root`, `chunk_slot_start_diff`)
+SETTINGS deduplicate_merge_projection_mode = 'rebuild'
+COMMENT 'Attestations first seen on the unfinalized chain broken down by 50ms chunks. Only includes attestations that were seen within 12000ms of the slot start time.';
+
+CREATE TABLE `${NETWORK_NAME}`.fct_attestation_first_seen_chunked_50ms ON CLUSTER '{cluster}' AS `${NETWORK_NAME}`.fct_attestation_first_seen_chunked_50ms_local ENGINE = Distributed(
+    '{cluster}',
+    '${NETWORK_NAME}',
+    fct_attestation_first_seen_chunked_50ms_local,
+    cityHash64(`slot_start_date_time`, `block_root`)
+);
+
+ALTER TABLE `${NETWORK_NAME}`.fct_attestation_first_seen_chunked_50ms_local
+ADD PROJECTION p_by_slot
+(
+    SELECT *
+    ORDER BY (`slot`, `block_root`, `chunk_slot_start_diff`)
+);
