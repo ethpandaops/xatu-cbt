aec5522fd97f2140008128366028c9571479e420
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-10-13 18:44:42 +1000

    change to admin_cbt prefx tables

--- migrations/001_admin.down.sql
+++ migrations/001_admin.down.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_incremental ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_incremental_local ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_scheduled ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_scheduled_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_cbt_incremental ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_cbt_incremental_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_cbt_scheduled ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.admin_cbt_scheduled_local ON CLUSTER '{cluster}';
--- migrations/001_admin.up.sql
+++ migrations/001_admin.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.cbt_incremental_local ON CLUSTER '{cluster}' (
+CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.admin_cbt_incremental_local ON CLUSTER '{cluster}' (
     updated_date_time DateTime(3) CODEC(DoubleDelta, ZSTD(1)),
     database LowCardinality(String) COMMENT 'The database name',
     table LowCardinality(String) COMMENT 'The table name',
@@ -12,15 +12,15 @@ CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.cbt_incremental_local ON CLUSTER '{cl
 )
 ORDER BY (database, table, position);
 
-CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.cbt_incremental ON CLUSTER '{cluster}' AS ${NETWORK_NAME}.cbt_incremental_local
+CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.admin_cbt_incremental ON CLUSTER '{cluster}' AS ${NETWORK_NAME}.admin_cbt_incremental_local
 ENGINE = Distributed(
     '{cluster}',
     '${NETWORK_NAME}',
-    'cbt_incremental_local',
+    'admin_cbt_incremental_local',
     cityHash64(database, table)
 );
 
-CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.cbt_scheduled_local ON CLUSTER '{cluster}' (
+CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.admin_cbt_scheduled_local ON CLUSTER '{cluster}' (
     updated_date_time DateTime(3) CODEC(DoubleDelta, ZSTD(1)),
     database LowCardinality(String) COMMENT 'The database name',
     table LowCardinality(String) COMMENT 'The table name',
@@ -33,10 +33,10 @@ CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.cbt_scheduled_local ON CLUSTER '{clus
 )
 ORDER BY (database, table);
 
-CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.cbt_scheduled ON CLUSTER '{cluster}' AS ${NETWORK_NAME}.cbt_scheduled_local
+CREATE TABLE IF NOT EXISTS ${NETWORK_NAME}.admin_cbt_scheduled ON CLUSTER '{cluster}' AS ${NETWORK_NAME}.admin_cbt_scheduled_local
 ENGINE = Distributed(
     '{cluster}',
     '${NETWORK_NAME}',
-    'cbt_scheduled_local',
+    'admin_cbt_scheduled_local',
     cityHash64(database, table)
 );
