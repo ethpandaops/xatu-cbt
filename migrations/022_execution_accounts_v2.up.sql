cb5c0c212236a370adf860bdb644edb28cdbea3f
weiihann <weihan774237@gmail.com>
2025-10-20 17:33:18 +0800

    feat: add int_pre_6780_accounts_destructs table

--- migrations/022_execution_accounts_v2.up.sql
+++ migrations/022_execution_accounts_v2.up.sql
@@ -35,3 +35,22 @@ CREATE TABLE `${NETWORK_NAME}`.int_address_reads ON CLUSTER '{cluster}' AS `${NE
     int_address_reads_local,
     cityHash64(`address`)
 );
+
+CREATE TABLE `${NETWORK_NAME}`.int_pre_6780_accounts_destructs_local on cluster '{cluster}' (
+    `address` String COMMENT 'The address of the account' CODEC(ZSTD(1)),
+    `block_number` UInt32 COMMENT 'The block number of the self-destructs' CODEC(ZSTD(1)),
+    `transaction_hash` FixedString(66) COMMENT 'The transaction hash' CODEC(ZSTD(1)),
+    `transaction_index` UInt64 COMMENT 'The transaction index' CODEC(DoubleDelta, ZSTD(1)),
+) ENGINE = ReplicatedMergeTree(
+    '/clickhouse/{installation}/{cluster}/tables/{shard}/{database}/{table}',
+    '{replica}'
+) PARTITION BY cityHash64(`address`) % 16
+ORDER BY
+    (address, block_number, transaction_hash) COMMENT 'Table for accounts self-destructs data pre-6780 (Dencun fork)';
+
+CREATE TABLE `${NETWORK_NAME}`.int_pre_6780_accounts_destructs ON CLUSTER '{cluster}' AS `${NETWORK_NAME}`.int_pre_6780_accounts_destructs_local ENGINE = Distributed(
+    '{cluster}',
+    `${NETWORK_NAME}`,
+    int_pre_6780_accounts_destructs_local,
+    cityHash64(`address`)
+);
