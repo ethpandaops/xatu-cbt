b1e19b7d56f267d115f741279968999def0f99de
weiihann <weihan774237@gmail.com>
2025-10-21 13:27:41 +0800

    feat: add int_accounts_alive

--- migrations/022_execution_accounts_v2.down.sql
+++ migrations/022_execution_accounts_v2.down.sql
@@ -3,4 +3,8 @@ DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_address_diffs_local ON CLUSTER '{clus
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_address_reads ON CLUSTER '{cluster}' SYNC;
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_address_reads_local ON CLUSTER '{cluster}' SYNC;
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_pre_6780_accounts_destructs ON CLUSTER '{cluster}' SYNC;
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_pre_6780_accounts_destructs_local ON CLUSTER '{cluster}' SYNC; 
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_pre_6780_accounts_destructs_local ON CLUSTER '{cluster}' SYNC;
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_post_6780_accounts_destructs ON CLUSTER '{cluster}' SYNC;
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_post_6780_accounts_destructs_local ON CLUSTER '{cluster}' SYNC;
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_accounts_alive ON CLUSTER '{cluster}' SYNC;
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.int_accounts_alive_local ON CLUSTER '{cluster}' SYNC;
