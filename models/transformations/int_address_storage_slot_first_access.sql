cc6b20afa82f903c405584cc931b92fb3d7c7443
weiihann <weihan774237@gmail.com>
2025-10-21 13:44:17 +0800

    refactor: storage slot first and last access

--- models/transformations/int_address_storage_slot_first_access.sql
+++ models/transformations/int_address_storage_slot_first_access.sql
@@ -11,39 +11,49 @@ tags:
   - address
   - storage
 dependencies:
+  - "{{external}}.canonical_execution_transaction"
   - "{{external}}.canonical_execution_storage_diffs"
   - "{{external}}.canonical_execution_storage_reads"
 ---
 INSERT INTO
   `{{ .self.database }}`.`{{ .self.table }}`
-WITH all_storage_data AS (
-    SELECT
-        lower(address) as address,
-        slot AS slot_key,
-        block_number AS bn,
-        transaction_index,
-        internal_index,
-        to_value AS value
-    FROM `{{ index .dep "{{external}}" "canonical_execution_storage_diffs" "database" }}`.`canonical_execution_storage_diffs` FINAL
-    WHERE block_number BETWEEN {{ .bounds.start }} AND {{ .bounds.end }}
-    
-    UNION ALL
-    
-    SELECT
-        lower(contract_address) as address,
-        slot AS slot_key,
-        block_number AS bn,
-        transaction_index,
-        internal_index,
-        value
-    FROM `{{ index .dep "{{external}}" "canonical_execution_storage_reads" "database" }}`.`canonical_execution_storage_reads` FINAL
-    WHERE block_number BETWEEN {{ .bounds.start }} AND {{ .bounds.end }}
+WITH 
+get_tx_success AS (
+  SELECT lower(transaction_hash) AS transaction_hash
+  FROM `{{ index .dep "{{external}}" "canonical_execution_transaction" "database" }}`.`canonical_execution_transaction` FINAL
+  WHERE block_number BETWEEN {{ .bounds.start }} AND {{ .bounds.end }}
+  AND success = true
+),
+all_storage_data AS (
+  SELECT
+    lower(address) AS address,
+    slot AS slot_key,
+    block_number AS bn,
+    transaction_index,
+    internal_index,
+    to_value AS value
+  FROM `{{ index .dep "{{external}}" "canonical_execution_storage_diffs" "database" }}`.`canonical_execution_storage_diffs` FINAL
+  WHERE block_number BETWEEN {{ .bounds.start }} AND {{ .bounds.end }}
+
+  UNION ALL
+
+  SELECT
+    lower(sr.contract_address) AS address,
+    sr.slot AS slot_key,
+    sr.block_number AS bn,
+    4294967295 AS transaction_index,
+    4294967295 AS internal_index,
+    sr.value AS value
+  FROM `{{ index .dep "{{external}}" "canonical_execution_storage_reads" "database" }}`.`canonical_execution_storage_reads` sr FINAL
+  GLOBAL JOIN get_tx_success g
+    ON lower(sr.transaction_hash) = g.transaction_hash
+  WHERE sr.block_number BETWEEN {{ .bounds.start }} AND {{ .bounds.end }}
 )
 SELECT
-    address,
-    slot_key AS slot,
-    argMin(bn, (bn, transaction_index, internal_index)) AS block_number,
-    argMin(value, (bn, transaction_index, internal_index)) AS value,
-    null AS `version`
+  address,
+  slot_key AS slot,
+  argMin(bn, (bn, transaction_index, internal_index)) AS block_number,
+  argMin(value, (bn, transaction_index, internal_index)) AS value,
+  null AS `version`
 FROM all_storage_data
 GROUP BY address, slot_key;
