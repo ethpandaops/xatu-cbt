0c9ec51fe83af192bdd6d05601a5647719f77604
Ng Wei Han <47109095+weiihann@users.noreply.github.com>
2025-10-23 10:21:04 +0700

    Apply suggestions from code review
    chore: remove comma
    
    Co-authored-by: Andrew Davis <1709934+Savid@users.noreply.github.com>
    Signed-off-by: Ng Wei Han <47109095+weiihann@users.noreply.github.com>

--- migrations/022_execution_accounts_v2.up.sql
+++ migrations/022_execution_accounts_v2.up.sql
@@ -2,10 +2,10 @@ CREATE TABLE `${NETWORK_NAME}`.int_address_diffs_local on cluster '{cluster}' (
     `address` String COMMENT 'The address of the account' CODEC(ZSTD(1)),
     `block_number` UInt32 COMMENT 'The block number of the diffs' CODEC(ZSTD(1)),
     `tx_count` UInt32 COMMENT 'The number of transactions with diffs for this address in the block' CODEC(ZSTD(1)),
-    `last_tx_index` UInt32 COMMENT 'The last transaction index with diffs for this address in the block' CODEC(ZSTD(1)),
+    `last_tx_index` UInt32 COMMENT 'The last transaction index with diffs for this address in the block' CODEC(ZSTD(1))
 ) ENGINE = ReplicatedMergeTree(
     '/clickhouse/{installation}/{cluster}/tables/{shard}/{database}/{table}',
-    '{replica}',
+    '{replica}'
 ) PARTITION BY cityHash64(`address`) % 16
 ORDER BY
     (address) COMMENT 'Table for accounts last access data';
@@ -21,7 +21,7 @@ CREATE TABLE `${NETWORK_NAME}`.int_address_reads_local on cluster '{cluster}' (
     `address` String COMMENT 'The address of the account' CODEC(ZSTD(1)),
     `block_number` UInt32 COMMENT 'The block number of the reads' CODEC(ZSTD(1)),
     `tx_count` UInt32 COMMENT 'The number of reads for this address in this block' CODEC(ZSTD(1)),
-    `last_tx_index` UInt32 COMMENT 'The last transaction index with diffs for this address in the block' CODEC(ZSTD(1)),
+    `last_tx_index` UInt32 COMMENT 'The last transaction index with diffs for this address in the block' CODEC(ZSTD(1))
 ) ENGINE = ReplicatedMergeTree(
     '/clickhouse/{installation}/{cluster}/tables/{shard}/{database}/{table}',
     '{replica}'
@@ -40,7 +40,7 @@ CREATE TABLE `${NETWORK_NAME}`.int_pre_6780_accounts_destructs_local on cluster 
     `address` String COMMENT 'The address of the account' CODEC(ZSTD(1)),
     `block_number` UInt32 COMMENT 'The block number of the self-destructs' CODEC(ZSTD(1)),
     `transaction_hash` FixedString(66) COMMENT 'The transaction hash' CODEC(ZSTD(1)),
-    `transaction_index` UInt64 COMMENT 'The transaction index' CODEC(DoubleDelta, ZSTD(1)),
+    `transaction_index` UInt64 COMMENT 'The transaction index' CODEC(DoubleDelta, ZSTD(1))
 ) ENGINE = ReplicatedMergeTree(
     '/clickhouse/{installation}/{cluster}/tables/{shard}/{database}/{table}',
     '{replica}'
@@ -60,7 +60,7 @@ CREATE TABLE `${NETWORK_NAME}`.int_post_6780_accounts_destructs_local on cluster
     `block_number` UInt32 COMMENT 'The block number' CODEC(ZSTD(1)),
     `transaction_hash` FixedString(66) COMMENT 'The transaction hash' CODEC(ZSTD(1)),
     `transaction_index` UInt64 COMMENT 'The transaction index' CODEC(DoubleDelta, ZSTD(1)),
-    `is_same_tx` Bool COMMENT 'Whether the self-destruct is in the same transaction as the creation' CODEC(ZSTD(1)),
+    `is_same_tx` Bool COMMENT 'Whether the self-destruct is in the same transaction as the creation' CODEC(ZSTD(1))
 ) ENGINE = ReplicatedMergeTree(
     '/clickhouse/{installation}/{cluster}/tables/{shard}/{database}/{table}',
     '{replica}'
