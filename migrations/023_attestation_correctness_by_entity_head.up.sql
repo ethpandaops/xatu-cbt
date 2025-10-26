9ac4b1dfd2ca9a0b0a038949813d1ab9330fc343
Sam Calder-Mason <sam@puritydev.io>
2025-10-26 22:24:58 +1000

    refactor(migration): simplify comment in attestation correctness table creation SQL

--- migrations/023_attestation_correctness_by_entity_head.up.sql
+++ migrations/023_attestation_correctness_by_entity_head.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE `${NETWORK_NAME}`.fct_attestation_correctness_by_entity_head_local 
     `slot_start_date_time` DateTime COMMENT 'The wall clock time when the slot started' CODEC(DoubleDelta, ZSTD(1)),
     `epoch` UInt32 COMMENT 'The epoch number containing the slot' CODEC(DoubleDelta, ZSTD(1)),
     `epoch_start_date_time` DateTime COMMENT 'The wall clock time when the epoch started' CODEC(DoubleDelta, ZSTD(1)),
-    `entity` Nullable(String) COMMENT 'The entity (staking provider) associated with the validators' CODEC(ZSTD(1)),
+    `entity` Nullable(String) COMMENT 'The entity associated with the validators' CODEC(ZSTD(1)),
     `block_root` Nullable(String) COMMENT 'The beacon block root hash that was attested, null means the attestation was missed' CODEC(ZSTD(1)),
     `attestation_count` UInt32 COMMENT 'Number of attestations for this entity/block_root combination' CODEC(ZSTD(1))
 ) ENGINE = ReplicatedReplacingMergeTree(
@@ -15,7 +15,7 @@ CREATE TABLE `${NETWORK_NAME}`.fct_attestation_correctness_by_entity_head_local 
 ORDER BY
     (`slot`, ifNull(`block_root`, ''), ifNull(`entity`, ''))
 SETTINGS deduplicate_merge_projection_mode = 'rebuild'
-COMMENT 'Attestation correctness aggregated by entity for the head chain. Multiple rows per slot when entities attest to different block roots (forks)';
+COMMENT 'Attestation correctness aggregated by entity for the head chain.';
 
 CREATE TABLE `${NETWORK_NAME}`.fct_attestation_correctness_by_entity_head ON CLUSTER '{cluster}' AS `${NETWORK_NAME}`.fct_attestation_correctness_by_entity_head_local ENGINE = Distributed(
     '{cluster}',
