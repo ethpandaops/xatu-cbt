d75732aa20afaa2b146504cd6147d717e1456cd3
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-12-02 16:12:55 +1000

    feat: add dim_block_blob_submitter transformation model
    Add a new transformation model that maps blob transactions to their
    submitter names. Includes:
    
    - External models for blob_submitter and execution_transaction tables
    - Scheduled transformation that joins type-3 transactions with
      blob submitter lookup data
    - Migration for the dim_block_blob_submitter table with partitioning
      by block number

---
+++ models/external/execution_transaction.sql
@@ -0,0 +1,25 @@
+---
+table: execution_transaction
+cache:
+  incremental_scan_interval: 5s
+  full_scan_interval: 24h
+interval:
+  type: block
+lag: 1
+---
+SELECT
+    {{ if .cache.is_incremental_scan }}
+      '{{ .cache.previous_min }}' as min,
+    {{ else }}
+      min(block_number) as min,
+    {{ end }}
+    max(block_number) as max
+FROM {{ .self.helpers.from }}
+WHERE 
+    meta_network_name = '{{ .env.NETWORK }}'
+
+{{ if .cache.is_incremental_scan }}
+  AND block_number >= {{ .cache.previous_max }}
+{{ else }}
+  AND block_number >= {{ default "0" .env.EXTERNAL_MODEL_MIN_BLOCK }}
+{{ end }}
