34b288d06df170302b5485ebff4ef48563a4abcd
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-12-03 14:32:32 +1000

    feat(dim_block_blob_submitter): add transaction_index and versioned_hashes columns (#149)
    - Add transaction_index and versioned_hashes columns to the table schema
    - Change ORDER BY from (block_number, transaction_hash) to (block_number, transaction_index)
    - Update distributed table sharding key to use transaction_index
    - Fix column references in transformation SQL to match source table

--- migrations/033_dim_block_blob_submitter.up.sql
+++ migrations/033_dim_block_blob_submitter.up.sql
@@ -2,14 +2,16 @@ CREATE TABLE `${NETWORK_NAME}`.dim_block_blob_submitter_local ON CLUSTER '{clust
     `updated_date_time` DateTime COMMENT 'Timestamp when the record was last updated' CODEC(DoubleDelta, ZSTD(1)),
     `block_number` UInt64 COMMENT 'The block number' CODEC(DoubleDelta, ZSTD(1)),
     `transaction_hash` FixedString(66) COMMENT 'The transaction hash' CODEC(ZSTD(1)),
+    `transaction_index` UInt32 COMMENT 'The transaction index' CODEC(DoubleDelta, ZSTD(1)),
     `address` String COMMENT 'The blob submitter address' CODEC(ZSTD(1)),
+    `versioned_hashes` Array(String) COMMENT 'The versioned hashes of the blob submitter' CODEC(ZSTD(1)),
     `name` Nullable(String) COMMENT 'The name of the blob submitter' CODEC(ZSTD(1))
 ) ENGINE = ReplicatedReplacingMergeTree(
     '/clickhouse/{installation}/{cluster}/tables/{shard}/{database}/{table}',
     '{replica}',
     `updated_date_time`
 ) PARTITION BY intDiv(block_number, 500000)
-ORDER BY (block_number, transaction_hash)
+ORDER BY (block_number, transaction_index)
 SETTINGS
     deduplicate_merge_projection_mode = 'rebuild'
 COMMENT 'Blob transaction to submitter name mapping.';
@@ -18,5 +20,5 @@ CREATE TABLE `${NETWORK_NAME}`.dim_block_blob_submitter ON CLUSTER '{cluster}' A
     '{cluster}',
     '${NETWORK_NAME}',
     dim_block_blob_submitter_local,
-    cityHash64(`block_number`, `transaction_hash`)
+    cityHash64(`block_number`, `transaction_index`)
 );
