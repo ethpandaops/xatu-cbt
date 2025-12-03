c41b4918eb56097d3678abac54120a25b6745f1c
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-12-03 15:46:26 +1000

    fix down migration

--- migrations/033_dim_block_blob_submitter.down.sql
+++ migrations/033_dim_block_blob_submitter.down.sql
@@ -1,2 +1,2 @@
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.dim_transaction_blob_submitter ON CLUSTER '{cluster}' SYNC;
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.dim_transaction_blob_submitter_local ON CLUSTER '{cluster}' SYNC;
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.dim_block_blob_submitter ON CLUSTER '{cluster}' SYNC;
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.dim_block_blob_submitter_local ON CLUSTER '{cluster}' SYNC;
