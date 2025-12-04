8bf6af3a973f7d6db200897ed0d6d12635ec07db
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-12-05 06:44:03 +1000

    refactor: simplify external model incremental scan timestamp logic
    Consolidate the conditional timestamp logic in external models to use
    a single approach that defaults to EXTERNAL_MODEL_MIN_TIMESTAMP (or 0)
    and overrides with previous_max when doing an incremental scan.

--- models/external/libp2p_gossipsub_blob_sidecar.sql
+++ models/external/libp2p_gossipsub_blob_sidecar.sql
@@ -18,8 +18,11 @@ FROM {{ .self.helpers.from }}
 WHERE 
     meta_network_name = '{{ .env.NETWORK }}'
 
-{{ if .cache.is_incremental_scan }}
-  AND slot_start_date_time >= fromUnixTimestamp({{ .cache.previous_max }})
-{{ else }}
-  AND slot_start_date_time >= fromUnixTimestamp({{ default "0" .env.EXTERNAL_MODEL_MIN_TIMESTAMP }})
-{{ end }}
+    -- previous_max if incremental scan and is set, otherwise default/env
+    {{- $ts := default "0" .env.EXTERNAL_MODEL_MIN_TIMESTAMP -}}
+    {{- if .cache.is_incremental_scan -}}
+      {{- if .cache.previous_max -}}
+        {{- $ts = .cache.previous_max -}}
+      {{- end -}}
+    {{- end }}
+    AND slot_start_date_time >= fromUnixTimestamp({{ $ts }})
