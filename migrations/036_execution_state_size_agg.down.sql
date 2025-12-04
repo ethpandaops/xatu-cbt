25319bdef46dac42cc8c8261624f2834a5e405a4
weiihann <weihan774237@gmail.com>
2025-12-04 16:05:31 +0800

    refactor(state): use separate tables for the deltas

--- migrations/036_execution_state_size_agg.down.sql
+++ migrations/036_execution_state_size_agg.down.sql
@@ -1,6 +1,12 @@
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_hourly_local ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_hourly ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_hourly_delta_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_hourly_delta ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_daily_local ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_daily ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_daily_delta_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_daily_delta ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_monthly_local ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_monthly ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_monthly_delta_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_monthly_delta ON CLUSTER '{cluster}';
--- models/transformations/fct_execution_state_size_daily.sql
+++ models/transformations/fct_execution_state_size_daily.sql
@@ -4,38 +4,34 @@ type: incremental
 interval:
   type: block
   max: 100000
-schedules:
-  forwardfill: "@every 5m"
-  backfill: "@every 1m"
+schedule: "@every 5m"
 tags:
   - daily
   - execution
   - state_size
 dependencies:
-  - "{{transformation}}.fct_execution_state_size_hourly"
+  - "{{external}}.execution_state_size"
+  - "{{external}}.canonical_execution_block"
 ---
 INSERT INTO `{{ .self.database }}`.`{{ .self.table }}`
 SELECT
     fromUnixTimestamp({{ .task.start }}) as updated_date_time,
-    toDate(hour_start_date_time) AS date,
-    count(*) AS hour_count,
-    min(min_block_number) AS min_block_number,
-    max(max_block_number) AS max_block_number,
-    argMax(accounts, hour_start_date_time) AS accounts,
-    argMax(account_bytes, hour_start_date_time) AS account_bytes,
-    argMax(account_trienodes, hour_start_date_time) AS account_trienodes,
-    argMax(account_trienode_bytes, hour_start_date_time) AS account_trienode_bytes,
-    argMax(contract_codes, hour_start_date_time) AS contract_codes,
-    argMax(contract_code_bytes, hour_start_date_time) AS contract_code_bytes,
-    argMax(storages, hour_start_date_time) AS storages,
-    argMax(storage_bytes, hour_start_date_time) AS storage_bytes,
-    argMax(storage_trienodes, hour_start_date_time) AS storage_trienodes,
-    argMax(storage_trienode_bytes, hour_start_date_time) AS storage_trienode_bytes,
-    sum(accounts_delta) AS accounts_delta,
-    sum(account_bytes_delta) AS account_bytes_delta,
-    sum(storage_bytes_delta) AS storage_bytes_delta,
-    sum(contract_code_bytes_delta) AS contract_code_bytes_delta,
-    argMax(total_bytes, hour_start_date_time) AS total_bytes
-FROM {{ index .dep "{{transformation}}" "fct_execution_state_size_hourly" "helpers" "from" }} FINAL
-WHERE max_block_number BETWEEN {{ .bounds.start }} AND {{ .bounds.end }}
-GROUP BY toDate(hour_start_date_time)
+    toDate(b.block_date_time) AS date,
+    argMax(s.accounts, s.block_number) AS account_count,
+    argMax(s.account_bytes, s.block_number) AS account_bytes,
+    argMax(s.account_trienodes, s.block_number) AS account_trienodes,
+    argMax(s.account_trienode_bytes, s.block_number) AS account_trienode_bytes,
+    argMax(s.contract_codes, s.block_number) AS contract_codes,
+    argMax(s.contract_code_bytes, s.block_number) AS contract_code_bytes,
+    argMax(s.storages, s.block_number) AS storage_count,
+    argMax(s.storage_bytes, s.block_number) AS storage_bytes,
+    argMax(s.storage_trienodes, s.block_number) AS storage_trienodes,
+    argMax(s.storage_trienode_bytes, s.block_number) AS storage_trienode_bytes,
+    argMax(s.account_trienode_bytes, s.block_number)
+        + argMax(s.contract_code_bytes, s.block_number)
+        + argMax(s.storage_trienode_bytes, s.block_number) AS total_bytes
+FROM {{ index .dep "{{external}}" "execution_state_size" "helpers" "from" }} AS s FINAL
+GLOBAL INNER JOIN {{ index .dep "{{external}}" "canonical_execution_block" "helpers" "from" }} AS b FINAL
+    ON s.block_number = b.block_number
+WHERE s.block_number BETWEEN {{ .bounds.start }} AND {{ .bounds.end }}
+GROUP BY toDate(b.block_date_time)
