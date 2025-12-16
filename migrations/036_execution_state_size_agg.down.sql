83584231c8cafd661a30494b46e764d6421db157
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-12-16 17:00:52 +1000

    remove state size down

--- migrations/036_execution_state_size_agg.down.sql
+++ migrations/036_execution_state_size_agg.down.sql
@@ -1,8 +1,4 @@
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_hourly_local ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_hourly ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_weekly_local ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_weekly ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_daily_local ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_daily ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_monthly_local ON CLUSTER '{cluster}';
-DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_execution_state_size_monthly ON CLUSTER '{cluster}';
