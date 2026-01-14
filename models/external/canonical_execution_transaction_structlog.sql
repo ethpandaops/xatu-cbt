6a93686ae614fb9592019729379404b23f197056
Matty Evans <mattevansnz@gmail.com>
2026-01-15 05:45:09 +1000

    refactor(canonical_execution_transaction_structlog.sql): replace slow ORDER BY LIMIT 1 bounds with aggregate MIN/MAX refactor(canonical_execution_transaction_structlog.sql): remove explicit timeout settings and simplify query chore(canonical_execution_transaction_structlog.sql): increase lag from 64 to 384 blocks to reduce re-org risk

--- models/external/canonical_execution_transaction_structlog.sql
+++ models/external/canonical_execution_transaction_structlog.sql
@@ -5,40 +5,27 @@ cache:
   full_scan_interval: 24h
 interval:
   type: block
-lag: 64
+lag: 384
 ---
--- This table is very large, so bounds queries are slow (~15s).
--- Using ORDER BY LIMIT 1 with generous timeouts.
 SELECT
-    {{ if .cache.is_incremental_scan }}
-      toUInt64('{{ .cache.previous_min }}') as min,
-    {{ else }}
-      (
-        SELECT block_number
-        FROM {{ .self.helpers.from }}
-        WHERE meta_network_name = '{{ .env.NETWORK }}'
-        ORDER BY block_number ASC
-        LIMIT 1
-      ) as min,
-    {{ end }}
-    (
-        SELECT block_number
-        FROM {{ .self.helpers.from }}
-        WHERE meta_network_name = '{{ .env.NETWORK }}'
-          {{- $bn := default "0" .env.EXTERNAL_MODEL_MIN_BLOCK -}}
-          {{- if .cache.is_incremental_scan -}}
-            {{- if .cache.previous_max -}}
-              {{- $bn = .cache.previous_max -}}
-            {{- end -}}
-          {{- end }}
-          AND block_number >= {{ $bn }}
-          {{- if .cache.is_incremental_scan }}
-            AND block_number <= {{ $bn }} + {{ default "10000" .env.EXTERNAL_MODEL_SCAN_SIZE_BLOCK }}
-          {{- end }}
-        ORDER BY block_number DESC
-        LIMIT 1
-    ) as max
-SETTINGS
-    connect_timeout_with_failover_ms = 60000,
-    receive_timeout = 120,
-    send_timeout = 120
+  {{ if .cache.is_incremental_scan }}
+  '{{ .cache.previous_min }}' as min,
+  {{ else }}
+  min(block_number) as min,
+  {{ end }}
+    max(block_number) as max
+FROM {{ .self.helpers.from }}
+WHERE
+    meta_network_name = '{{ .env.NETWORK }}'
+
+    -- previous_max if incremental scan and is set, otherwise default/env
+    {{- $bn := default "0" .env.EXTERNAL_MODEL_MIN_BLOCK -}}
+    {{- if .cache.is_incremental_scan -}}
+      {{- if .cache.previous_max -}}
+        {{- $bn = .cache.previous_max -}}
+      {{- end -}}
+    {{- end }}
+    AND block_number >= {{ $bn }}
+    {{- if .cache.is_incremental_scan }}
+      AND block_number <= {{ $bn }} + {{ default "10000" .env.EXTERNAL_MODEL_SCAN_SIZE_BLOCK }}
+    {{- end }}
