19758a5245de6843db0ce6fc4072a50190398b4f
Matty Evans <mattevansnz@gmail.com>
2026-01-15 06:35:08 +1000

    refactor(canonical_execution_transaction.sql): rename table to canonical_execution_transaction_structlog chore(canonical_execution_transaction.sql): remove trailing whitespace

--- models/external/canonical_execution_transaction.sql
+++ models/external/canonical_execution_transaction.sql
@@ -1,5 +1,5 @@
 ---
-table: canonical_execution_transaction
+table: canonical_execution_transaction_structlog
 cache:
   incremental_scan_interval: 1m
   full_scan_interval: 24h
@@ -15,7 +15,7 @@ SELECT
     {{ end }}
     max(block_number) as max
 FROM {{ .self.helpers.from }}
-WHERE 
+WHERE
     meta_network_name = '{{ .env.NETWORK }}'
 
     -- previous_max if incremental scan and is set, otherwise default/env
