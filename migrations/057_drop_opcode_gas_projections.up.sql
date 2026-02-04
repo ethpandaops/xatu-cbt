6749440f00a8938840fc36ccac1e85a6b97f34ee
Matty Evans <mattevansnz@gmail.com>
2026-02-05 08:05:34 +1000

    refactor(migrations): drop unused projections and columns from opcode gas table

--- migrations/057_drop_opcode_gas_projections.up.sql
+++ migrations/057_drop_opcode_gas_projections.up.sql
@@ -1,8 +1,5 @@
 -- Drop unused projections from int_transaction_call_frame_opcode_gas_local
--- These projections tripled INSERT cost (data written 3x with different sort orders)
--- but are not used by any downstream transformations.
--- p_by_frame: used by frontend queries, can be re-added if needed
--- p_by_opcode: not used by any current query
+-- These projections add heavily to INSERT cost (and disk), and can we worked around.
 
 ALTER TABLE `${NETWORK_NAME}`.int_transaction_call_frame_opcode_gas_local ON CLUSTER '{cluster}'
     DROP PROJECTION IF EXISTS p_by_frame;
@@ -11,7 +8,6 @@ ALTER TABLE `${NETWORK_NAME}`.int_transaction_call_frame_opcode_gas_local ON CLU
     DROP PROJECTION IF EXISTS p_by_opcode;
 
 -- Drop unused min_depth/max_depth columns from int_transaction_call_frame_opcode_gas_local
--- These are not consumed by any downstream transformation or frontend query.
 
 ALTER TABLE `${NETWORK_NAME}`.int_transaction_call_frame_opcode_gas_local ON CLUSTER '{cluster}'
     DROP COLUMN IF EXISTS min_depth;
