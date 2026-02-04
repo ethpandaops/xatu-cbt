fc378a4579cb0d3edb4f007b2f9a3944bba04fea
Matty Evans <mattevansnz@gmail.com>
2026-02-05 08:03:30 +1000

    feat(migrations): add min_depth and max_depth columns to int_transaction_opcode_gas_local These columns track the minimum and maximum call stack depth reached by each opcode, enabling more precise gas profiling and debugging.

--- migrations/056_gas_profiling.up.sql
+++ migrations/056_gas_profiling.up.sql
@@ -18,6 +18,10 @@ CREATE TABLE `${NETWORK_NAME}`.int_transaction_opcode_gas_local ON CLUSTER '{clu
     `gas` UInt64 COMMENT 'Gas consumed by this opcode. sum(gas) = transaction executed gas' CODEC(ZSTD(1)),
     `gas_cumulative` UInt64 COMMENT 'For CALL opcodes: includes all descendant frame gas. For others: same as gas' CODEC(ZSTD(1)),
 
+    -- Call depth metrics
+    `min_depth` UInt64 COMMENT 'Minimum call stack depth for this opcode' CODEC(ZSTD(1)),
+    `max_depth` UInt64 COMMENT 'Maximum call stack depth for this opcode' CODEC(ZSTD(1)),
+
     -- Error tracking
     `error_count` UInt64 COMMENT 'Number of times this opcode resulted in an error' CODEC(ZSTD(1)),
 
