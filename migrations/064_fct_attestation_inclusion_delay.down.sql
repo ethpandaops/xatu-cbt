e3a8a6fbeec4783fa9ca98b296bbdc5af292950a
Sam Calder-Mason <sam@puritydev.io>
2026-02-10 09:29:58 +1000

    feat: add attestation inclusion delay hourly/daily models and migration

---
+++ migrations/064_fct_attestation_inclusion_delay.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_inclusion_delay_hourly ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_inclusion_delay_hourly_local ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_inclusion_delay_daily ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS `${NETWORK_NAME}`.fct_attestation_inclusion_delay_daily_local ON CLUSTER '{cluster}';
